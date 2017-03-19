3ec649a404b6dcf443dd6c8a0df9e253ff406fda
Alex Boffey <alex@alexboffey.co.uk>
2017-03-19 15:51:23 +0000

    Updated webpack conf & added meta viewport tag

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,8 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin('style.bundle.css'),
-    new UglifyJSPlugin({ compress: true })
+    new UglifyJSPlugin({
+      compress: { warnings: false }
+    })
   ]
 }
